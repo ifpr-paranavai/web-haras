46a7400b2279b21de91f3368899112f41445e134
Cristhian Maia <cristhian.maia@capgemini.com>
2022-04-26 22:23:09 -0300

    projeto backend criado

---
+++ api-webharas/src/test/java/com/api/apiwebharas/ApiWebharasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiwebharas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebharasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
